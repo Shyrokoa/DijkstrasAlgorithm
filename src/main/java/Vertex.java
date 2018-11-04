94c197df9f6f94bb0c9f17e85c63c1d83fd18928
Shyrokoa <44732261+Shyrokoa@users.noreply.github.com>
2018-11-04 19:13:51 +0100

    Created the Vertex java class.

---
+++ src/main/java/Vertex.java
@@ -0,0 +1,44 @@
+public class Vertex {
+    final private String id;
+    final private String name;
+
+    Vertex(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int primeNumber = 17;
+        int result = 1;
+        result = primeNumber * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vertex other = (Vertex) obj;
+        if (id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
